2b23fe088727ef11996158e7ddd2a6c87fda2d82
Julie Zimmer <julie.berthiaume@gmail.comgit config --global user.email julie.berthiaume@gmail.com>
2017-11-03 21:13:03 -0500

    video playback and modal complete

---
+++ src/js/main.js
@@ -0,0 +1,19 @@
+//code to autoplay a video
+$(function() {
+  // Auto play modal video using jQuery
+ 
+  /* Uses the class "video" from <a> tag in the video play section of index.html as a selector.
+  When the play icon is clicked, the function below runs.
+  The video is opened. 
+  theModal is the data-target for the function.
+  This plays one time (autoplay is set to one: autoplay=1) and the video plays in the modal. */ 
+  $(".video").click(function () {
+    var theModal = $(this).data("target"),
+    videoSRC = $(this).attr("data-video"),
+    videoSRCauto = videoSRC + "?modestbranding=1&rel=0&controls=0&showinfo=0&html5=1&autoplay=1";
+    $(theModal + ' iframe').attr('src', videoSRCauto);
+    $(theModal + ' button.close').click(function () {
+      $(theModal + ' iframe').attr('src', videoSRC);
+    });
+  });
+});
